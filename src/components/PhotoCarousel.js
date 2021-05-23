b07c65aa7dc6a1102f76cb5690e1bc58b5bb910a
unknown <itsaustinchen@gmail.com>
2021-05-23 19:11:30 -0400

    Fix splide error

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -4,6 +4,8 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { SRLWrapper } from "simple-react-lightbox";
 
 const PhotoCarousel = ({ images }) => {
+  if (typeof window === "undefined") return <p>Server render</p>;
+
   const [loadedImages, setLoadedImages] = useState({});
   const updateLoadedImages = (i) => {
     setLoadedImages({ ...loadedImages, [i]: true });
